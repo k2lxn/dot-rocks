a78af4d0dab05ca579a61c5cb9169cba083040c5
Jane Reynolds <jane.e.reynolds@dartmouth.edu>
2018-03-18 23:04:22 -0400

    Capture scroll direction

--- public/js/bg-scroll.js
+++ public/js/bg-scroll.js
@@ -1,5 +1,18 @@
 var total_slides;
+var scroll_direction;
+var last_scrollTop;
 
+function set_scroll_direction( last, curr ) {
+	if ( last > curr ) {
+		scroll_direction = "left";
+	}
+	else {
+		scroll_direction = "right";
+	}
+	console.log("scroll_direction: " + scroll_direction );
+
+	//last_scrollTop = curr;
+}
 
 
 function set_scroll() {
@@ -9,14 +22,20 @@ function set_scroll() {
 		var offset = $(".scroll-trigger-container").scrollTop();
 				
 		$("#slideshow").offset({top: 0, left: -offset });
+
+		set_scroll_direction( last_scrollTop, offset );
+
+		last_scrollTop = offset;
 	});
 }
-
 
 
 /* READY */
 $(document).ready(function(){
 	total_slides = 4;
+	scroll_direction = "right";
+	last_scrollTop = 0;
+
 	set_scroll();
 	
 });
