92ff62b28762ea2b1a381af4ab4da6256d3d6d2b
Jane Reynolds <jane.e.reynolds@dartmouth.edu>
2018-04-01 21:29:15 -0400

    Set up button triggered scrolling

--- public/js/bg-scroll.js
+++ public/js/bg-scroll.js
@@ -1,93 +1,50 @@
 var total_slides = 4;
 var slide_width;
-var scroll_direction;
-var last_scrollTop;
-var snap_to;
 
-function set_scroll_direction( last, curr ) {
-	if ( last > curr ) {
-		scroll_direction = "right";
-	}
-	else {
-		scroll_direction = "left";
-	}
-}
-
-
-function set_snap_to() {
-	if ( scroll_direction === "left" ) {
-		for ( i=1; i < total_slides + 1 ; i++ ) {	
-			if ( slide_width * i > last_scrollTop ) {
-				return ( slide_width * i ) ;
-			}
-		}
-
-	} else {
-		for ( i=total_slides; i > -1 ; i-- ) {	
-			if ( slide_width * i < last_scrollTop ) {
-				return ( slide_width * i ) ;
-			}
-		}
-	}
-}
-
-
-function scroll_to_snap() {
-	$(".scroll-trigger-container").unbind("scroll"); // temporarily disable scrolling
-
+function scroll_right(){
+	var curr_scroll = $("#slideshow").offset().left;
+	var scroll_to = curr_scroll - slide_width;
 	var scroll_speed = slide_width * .75;
 
-	$('#slideshow').animate({
-		left: -(snap_to)
-	}, scroll_speed, function(){
-		$(".scroll-trigger-container").scrollTop( snap_to );
-		last_scrollTop = snap_to ; 
-
-		// reenable scrolling after a short timeout
-		setTimeout(function(){
-			set_scroll();
-		}, 1000);
-	});
+	if ( Math.abs(scroll_to) < $('#slideshow').width() ) {
+		//$("#slideshow").offset({top: 0, left: scroll_to });
+		$('#slideshow').animate({
+			left: scroll_to
+		}, scroll_speed );
+	}
 }
 
-function set_scroll() {
-	$(".scroll-trigger").height( (window.innerWidth * total_slides) + (window.innerHeight - window.innerWidth) );
+function scroll_left(){
+	var curr_scroll = $("#slideshow").offset().left;
+	var scroll_to = curr_scroll + slide_width;
+	var scroll_speed = slide_width * .75;
 
-	$(".scroll-trigger-container").scroll(function(){
-		var offset = $(".scroll-trigger-container").scrollTop();
-
-		set_scroll_direction( last_scrollTop, offset );
-		last_scrollTop = offset;
-
-		$("#slideshow").offset({top: 0, left: -offset });
-
-		// if offset is within a certain distance of snap_to, snap_to
-		snap_to = set_snap_to();
-		if ( offset >= snap_to - slide_width * .9 ) {
-			scroll_to_snap();
-		}
-	});
+	if ( Math.abs(curr_scroll) >= slide_width ) {
+		//$("#slideshow").offset({top: 0, left: scroll_to });
+		$('#slideshow').animate({
+			left: scroll_to
+		}, scroll_speed );
+	}
 }
 
 
 /* READY */
 $(document).ready(function(){
 	slide_width = $('#slideshow').width() / total_slides ;
-	scroll_direction = "left";
-	last_scrollTop = 0;
-
-	snap_to = slide_width;
-
-	set_scroll();
 	
+	$("#scroll-right").click( scroll_right );
+	$("#scroll-left").click( scroll_left );
 });
+
 
 /* RESIZE */
 $(window).resize(function(){
+	var old_slide_width = slide_width ;
 	slide_width = $('#slideshow').width() / total_slides ;
 
-	//snap_to = set_snap_to();
-	//set_scroll();
+	// snap to slide edge
+	var slide_index = total_slides * ( Math.abs( document.getElementById("slideshow").offsetLeft ) / (old_slide_width * total_slides) ) ;
+	$("#slideshow").offset({top: 0, left: -( slide_index * slide_width ) });
 	
 });
 
