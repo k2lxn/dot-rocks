40c5bb1822a648c17dd8c3960f05fd08106ee3ec
Jane Reynolds <jane.e.reynolds@dartmouth.edu>
2018-05-23 22:31:59 -0400

    Setup mechanism for illustration exits + entrances

--- public/js/bg-scroll.js
+++ public/js/bg-scroll.js
@@ -1,5 +1,6 @@
 var total_slides = 4;
 var slide_width;
+var curr_slide;
 
 function scroll_right(){
 	var curr_scroll = $("#slideshow").offset().left;
@@ -12,8 +13,10 @@ function scroll_right(){
 			left: scroll_to
 		}, scroll_speed );
 
+		curr_slide++;
+
+		// hide/show scroll buttons
 		$("#scroll-left").show();
-		
 		if ( Math.abs(scroll_to) >= ( $('#slideshow').width() - slide_width) ) {
 			$("#scroll-right").hide();
 		}
@@ -32,20 +35,61 @@ function scroll_left(){
 		}, scroll_speed );
 	}
 
+	curr_slide--;
+
+	// hide/show scroll buttons
 	$("#scroll-right").show();
-	
 	if ( Math.abs(curr_scroll) <= slide_width ) {
 		$("#scroll-left").hide();
 	}
+}
+
+function stage_illustrations() {
+	console.log("curr_slide: " + curr_slide);
+	$(".illustration").each( function() {
+
+		var offstage_left =  ( slide_width / 2 ) + $(this).width() ;
+		var duration = slide_width * 1.5 ;
+		
+		// disappears
+		if ( $(this).data("disappearon") === curr_slide ) {
+			$(this).data("reappearon", curr_slide - 1 );
+			
+			$(this).animate({
+				left: -(offstage_left)
+			}, duration, function(){
+				$(this).css("visibility", "hidden");
+			});
+		}
+
+		// reappears
+		else if ( $(this).data("reappearon") === curr_slide )  {
+			$(this).css("visibility", "visible");
+
+			$(this).animate({
+				left: $(this).data("left")
+			}, duration );
+		}
+		
+	});
 }
 
 
 /* READY */
 $(document).ready(function(){
 	slide_width = $('#slideshow').width() / total_slides ;
+	curr_slide = 0;
+
+	stage_illustrations();
 	
-	$("#scroll-right").click( scroll_right );
-	$("#scroll-left").click( scroll_left );
+	$("#scroll-right").click( function() { 
+		scroll_right(); 
+		stage_illustrations();
+	} );
+	$("#scroll-left").click( function() { 
+		scroll_left(); 
+		stage_illustrations();
+	} );
 });
 
 
